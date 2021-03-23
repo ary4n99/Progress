3847e9fbc47492cf73c6269bd93ddf9a319e07f8
Aryan Agrawal <ary4n99@gmail.com>
2021-03-23 21:00:32 +0000

    Add test button

--- app/src/main/java/com/aryanagrawal/progress/MainActivity.kt
+++ app/src/main/java/com/aryanagrawal/progress/MainActivity.kt
@@ -2,10 +2,19 @@ package com.aryanagrawal.progress
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var buttonCounter = 0
+        button.setOnClickListener() {
+            buttonCounter += 1
+            textView.text = buttonCounter.toString()
+            Toast.makeText(this@MainActivity, "Clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
